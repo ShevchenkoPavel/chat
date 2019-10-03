8c84c3802da33f18d78ded36c142c50bbd9f0a8a
ShevchenkoPavel <shevchenkopavel17@gmail.com>
2019-10-04 02:01:28 +0600

    fetcher

--- src/utils/queries/fetcher.ts
+++ src/utils/queries/fetcher.ts
@@ -1,33 +1,21 @@
-function checkStatus(response: Response) {
-  if (response.status >= 200 && response.status < 300) {
-    return Promise.resolve(response);
-  }
-  return Promise.reject(new Error(response.statusText));
-}
-
-function getResponseJson(response: Response) {
-  return response.json();
-}
-
-export function get(url: string) {
-  return fetch(url, {
-    method: 'GET',
-    headers: new Headers({
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    })
+const DEFAULT_GET_METHOD_BODY = {
+  method: 'GET',
+  headers: new Headers({
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
   })
-    .then(response => checkStatus(response))
-    .then(json => getResponseJson(json))
-    .then(
-      data => {
-        return data;
-      },
-      data => {
-        return data;
-      }
-    )
-    .catch(error => {
-      return error;
-    });
+};
+
+function checkStatus(response: Response): boolean {
+  return response.status >= 200 && response.status < 300;
+}
+
+export async function get(url: string) {
+  try {
+    const response = await fetch(url, DEFAULT_GET_METHOD_BODY);
+    if (checkStatus) return await response.json();
+    // throw new Error(response.status);
+  } catch (err) {
+    return err;
+  }
 }
