393628f7c0fdfcc7e326c939f9a689d99236f285
ShevchenkoPavel <shevchenkopavel17@gmail.com>
2019-10-08 01:42:09 +0600

    [T-2] replace component state with react-hook

--- src/pages/chat/chat.tsx
+++ src/pages/chat/chat.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './chat.scss';
 import MockUsers from '../../utils/mock/users.json';
 
@@ -7,33 +7,20 @@ import { Container, Row, Col } from 'react-bootstrap';
 import { UsersBar } from '../../components/users_bar/users_bar';
 import { UserType } from '../../types/user';
 
-type Props = {};
-type State = {
-  users: Array<UserType>;
-};
+export function Chat(): React.ReactElement {
+  const [users] = useState<UserType[]>(MockUsers.users);
 
-export class Chat extends React.Component<Props, State> {
-  state = {
-    users: []
-  };
-
-  componentDidMount(): void {
-    this.setState({ users: MockUsers.users });
-  }
-
-  render(): React.ReactNode {
-    return (
-      <Container
-        className="chat__container"
-        style={{ paddingLeft: 0, paddingRight: 0 }}
-      >
-        <Row noGutters className="chat__container__content">
-          <Col xs={4}>
-            <UsersBar users={this.state.users} />
-          </Col>
-          <Col>MessagesWindow</Col>
-        </Row>
-      </Container>
-    );
-  }
+  return (
+    <Container
+      className="chat__container"
+      style={{ paddingLeft: 0, paddingRight: 0 }}
+    >
+      <Row noGutters className="chat__container__content">
+        <Col xs={4}>
+          <UsersBar users={users} />
+        </Col>
+        <Col>MessagesWindow</Col>
+      </Row>
+    </Container>
+  );
 }
