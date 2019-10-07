a4a687a7893f949b26d9932ccfcf0396c082e626
ShevchenkoPavel <shevchenkopavel17@gmail.com>
2019-10-08 01:01:17 +0600

    [T-3] add messages_window

---
+++ src/components/messages_window/messages_window.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './messages_window.scss';
+
+export const MessagesWindow = (): React.ReactElement => {
+  return (
+    <div className="messages-window">
+      <div className="messages-window__window">1</div>
+      <div className="messages-window__input">1</div>
+      <div className="messages-window__send">1</div>
+    </div>
+  );
+};
